ea02453a819f82ec5fe14ba82ea103b1d7c4e38f
Denis Wolter <wolter.important@gmail.com>
2022-08-06 12:49:32 +0300

    init, routing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Counter from "./components/Сounter/Counter";
+import Todo from "./components/Todo/Todo";
+import Trello from "./components/Trello/Trello";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App/>}/>
+      <Route path="/counter" element={<Counter/>}/>
+      <Route path="/todo" element={<Todo/>}/>
+      <Route path="/trello" element={<Trello/>}/>
+    </Routes>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
