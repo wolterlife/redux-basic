eb480e1e2c301e159259216683992d272457a35d
Denis Wolter <wolter.important@gmail.com>
2022-08-10 17:13:07 +0300

    create countRedux

--- src/components/Сounter/Counter.js
+++ src/components/Сounter/Counter.js
@@ -1,9 +1,15 @@
 import React from "react";
+import './Counter.css'
+import {useDispatch, useSelector, useStore} from "react-redux";
 
 const Counter = () => {
+  const count = useSelector(state => state)
+  const dispatch = useDispatch()
   return (
-    <div>
-      <p>Каунтер</p>
+    <div className={"counter"}>
+      <p className={'counter__text'}>{count}</p>
+      <button onClick={() => dispatch({ type: "DECREMENT" })} className={"counter__button"}>-</button>
+      <button onClick={() => dispatch({ type: "INCREMENT" })} className={"counter__button"}>+</button>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -5,15 +5,19 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Counter from "./components/Сounter/Counter";
 import Todo from "./components/Todo/Todo";
 import Trello from "./components/Trello/Trello";
+import {Provider} from "react-redux";
+import store from "./store/store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App/>}/>
-      <Route path="/counter" element={<Counter/>}/>
-      <Route path="/todo" element={<Todo/>}/>
-      <Route path="/trello" element={<Trello/>}/>
-    </Routes>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App/>}/>
+        <Route path="/counter" element={<Counter/>}/>
+        <Route path="/todo" element={<Todo/>}/>
+        <Route path="/trello" element={<Trello/>}/>
+      </Routes>
+    </BrowserRouter>
+  </Provider>
 );
